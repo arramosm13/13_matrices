3a0ea06cd51de43bfe634e45348bd320dd134f9c
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-15 10:58:27 -0500

    En el ejercicio 1 se esta intentando que funcione.

--- matrices1.cpp
+++ matrices1.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
+const int max=100;
+void leer(int n; int a[max][max]);
+void suma(int n; int a[max][max]);
 
 int main(){
-	int n;
+	int n, sum;
 	cout<<"Ingrese el tamaño de la matriz: "; cin>>n;
-	
-	
+	leer (n; a[max][max]);
+	sum = suma(n,a[max][max])<<endl
+	cout<<"La sumatoria es: "<<sum<<endl;
 	return 0;
+}
+void leer (int n; int a[max][max]){
+	for(j=0; j<n; j++){
+		for(i=0; i<n; i++){
+			cout<<"Ingrese el valor de ["<<i+1<<"] ["<<j+1<<"]."<<endl;
+			cin>>a[i][j];
+		}
+	}
+}
+int suma (int n; int a[max][max]){
+	int suma=0;
+	for (i=1; i=n){
+		for (j=1; j=n){
+			suma=suma+a[i][j]
+			return suma;
+		}
+	}
 }
