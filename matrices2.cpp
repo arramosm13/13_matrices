c655224cfef113f1a291e912f061e8a14a6ea466
arramosm13 <arramosm@unjbg.edu.pe>
2024-08-13 19:31:30 -0500

    agregue 2 for para que se pueda ingresar los valores de la matriz.

--- matrices2.cpp
+++ matrices2.cpp
@@ -4,5 +4,11 @@ using namespace std;
 int main (){
 	int n;
 	cout<<"Ingrese el tamaño de la matriz:", cin>> n;
-	
+	int A[n][n];
+	for (int i=0; i<n; i++){
+		for (int j=0; j<n; j++){
+			cout<<"Ingrese en valor de ["<<i+1<<"] ["<<j+1<<"]:"<< endl; 
+			cin>> A[i][j];
+		}
+	}
 }
